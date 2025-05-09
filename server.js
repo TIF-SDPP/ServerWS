45c498ec4185ef72e61fc1a5afa2d92e10ab880e
leoduville <71992403+leoduville@users.noreply.github.com>
2025-05-09 17:06:21 -0300

    cambios

--- server.js
+++ server.js
@@ -1,95 +1,42 @@
-const amqp = require("amqplib");
-const WebSocket = require("ws");
+  import amqp from "amqplib";
+  import WebSocket, { WebSocketServer } from "ws";
+  import 'dotenv/config'
 
-const RABBITMQ_URL = "amqp://guest:guest@34.74.155.113:5672";
-const EXCHANGE_NAME = "workers_queue";
-const ROUTING_KEY = "hash_task";
+  const RABBITMQ_URL = process.env.RABBITMQ_URL;
+  const EXCHANGE_NAME = process.env.EXCHANGE_NAME;
+  const ROUTING_KEY = process.env.ROUTING_KEY;
 
-// Crear servidor WebSocket
-const wss = new WebSocket.Server({ port: 80 });
+  const wss = new WebSocketServer({ port: 80 });
 
-async function start() {
-  try {
-    const conn = await amqp.connect(RABBITMQ_URL);
-    const ch = await conn.createChannel();
-    await ch.assertExchange(EXCHANGE_NAME, "topic", { durable: true });
-    const q = await ch.assertQueue("", { exclusive: true });
+  async function start() {
+    try {
+      const conn = await amqp.connect(RABBITMQ_URL);
+      const ch = await conn.createChannel();
+      await ch.assertExchange(EXCHANGE_NAME, "topic", { durable: true });
+      const q = await ch.assertQueue("", { exclusive: true });
 
-    await ch.bindQueue(q.queue, EXCHANGE_NAME, ROUTING_KEY);
+      await ch.bindQueue(q.queue, EXCHANGE_NAME, ROUTING_KEY);
 
-    console.log("Esperando mensajes de RabbitMQ...");
+      console.log("Esperando mensajes de RabbitMQ...");
 
-    ch.consume(q.queue, (msg) => {
-      if (msg) {
-        const data = JSON.parse(msg.content.toString());
-        console.log("Mensaje recibido:", data);
+      ch.consume(q.queue, (msg) => {
+        if (msg) {
+          const data = JSON.parse(msg.content.toString());
+          console.log("Mensaje recibido:", data);
 
-        // Enviar el mensaje a todos los clientes WebSocket
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(data));
-          }
-        });
+          // Enviar el mensaje a todos los clientes WebSocket
+          wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify(data));
+            }
+          });
 
-        ch.ack(msg);
-      }
-    });
-  } catch (error) {
-    console.error("Error conectando a RabbitMQ:", error);
+          ch.ack(msg);
+        }
+      });
+    } catch (error) {
+      console.error("Error conectando a RabbitMQ:", error);
+    }
   }
-}
 
-start();
-
-// const http = require("http");
-// const amqp = require("amqplib");
-// const WebSocket = require("ws");
-
-// const RABBITMQ_URL = "amqp://guest:guest@service-rabbitmq.default.svc.cluster.local:5672";
-// const EXCHANGE_NAME = "workers_queue";
-// const ROUTING_KEY = "hash_task";
-
-// const server = http.createServer(); // Servidor HTTP base
-// const wss = new WebSocket.Server({ server });
-
-// wss.on("connection", (ws) => {
-//   console.log("Cliente WebSocket conectado");
-// });
-
-// async function start() {
-//   try {
-//     const conn = await amqp.connect(RABBITMQ_URL);
-//     const ch = await conn.createChannel();
-//     await ch.assertExchange(EXCHANGE_NAME, "topic", { durable: true });
-//     const q = await ch.assertQueue("", { exclusive: true });
-
-//     await ch.bindQueue(q.queue, EXCHANGE_NAME, ROUTING_KEY);
-
-//     console.log("Esperando mensajes de RabbitMQ...");
-
-//     ch.consume(q.queue, (msg) => {
-//       if (msg) {
-//         const data = JSON.parse(msg.content.toString());
-//         console.log("Mensaje recibido:", data);
-
-//         // Difundir a todos los clientes conectados
-//         wss.clients.forEach((client) => {
-//           if (client.readyState === WebSocket.OPEN) {
-//             client.send(JSON.stringify(data));
-//           }
-//         });
-
-//         ch.ack(msg);
-//       }
-//     });
-
-//     // Iniciar el servidor HTTP en puerto 80 (requerido para el Service + Ingress)
-//     server.listen(80, () => {
-//       console.log("Servidor WebSocket escuchando en puerto 80 (/ws)");
-//     });
-//   } catch (error) {
-//     console.error("Error conectando a RabbitMQ:", error);
-//   }
-// }
-
-// start();
+  start();
